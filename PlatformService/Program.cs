6b66e5b5adb6cc9f50b11d1a79c4028b905e1fb0
Wojciech Gąsiewicz <gasiewicz.wojciech@gmail.com>
2023-12-11 18:40:45 +0100

    mssql db

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -10,14 +10,24 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
+if(builder.Environment.IsDevelopment())
+{    
+    builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
+    Console.WriteLine("--> using inmem db");
+}
+else
+{
+    Console.WriteLine("--> using sqlserver db");
+    builder.Services.AddDbContext<AppDbContext>(opt 
+    => opt.UseSqlServer(builder.Configuration.GetConnectionString("PlatformsConn")));
+}
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();//use of httpclient factory
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
-PrepDb.PrepPopulation(app);
+PrepDb.PrepPopulation(app, app.Environment.IsProduction());
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
